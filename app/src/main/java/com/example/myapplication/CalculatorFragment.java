d50ade3bfcf2927b1ff1e4e8001ba881528df579
Alex Tran <qut735@usask.ca>
2023-01-17 21:17:12 -0700

    Added onClick listeners to each of the buttons and created a fragment for the calulator

---
+++ app/src/main/java/com/example/myapplication/CalculatorFragment.java
@@ -0,0 +1,63 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+import android.view.View;
+
+public class CalculatorFragment extends Activity {
+    public void clearOnClick(View view) {
+    }
+
+    public void divideOnClick(View view) {
+    }
+
+    public void plusMinusOnClick(View view) {
+    }
+
+    public void evaluateOnClick(View view) {
+    }
+
+    public void multiplyOnClick(View view) {
+    }
+
+    public void addOnClick(View view) {
+    }
+
+    public void subtractOnClick(View view) {
+    }
+
+    public void sevenOnClick(View view) {
+    }
+
+    public void eightOnClick(View view) {
+    }
+
+    public void nineOnClick(View view) {
+    }
+
+    public void percentageOnClick(View view) {
+    }
+
+    public void twoOnClick(View view) {
+    }
+
+    public void zeroOnClick(View view) {
+    }
+
+    public void decimalOnClick(View view) {
+    }
+
+    public void fiveOnClick(View view) {
+    }
+
+    public void sixOnClick(View view) {
+    }
+
+    public void oneOnClick(View view) {
+    }
+
+    public void fourOnClick(View view) {
+    }
+
+    public void threeOnClick(View view) {
+    }
+}
